16f5f037ad8f112564ade235ff4fb1793bf92943
Ori <orirose10@gmail.com>
2021-02-24 11:55:37 +0200

    added mail components

---
+++ js/apps/mail/cmps/mail-filter.js
@@ -0,0 +1,10 @@
+export default {
+    template:`
+        <ul>
+            <li>Inbox</li>
+            <li>Starred</li>
+            <li>Sent Mail</li>
+            <li>Drafts</li>
+        </ul>
+    `
+}
--- js/apps/mail/pages/mail-app.cmp.js
+++ js/apps/mail/pages/mail-app.cmp.js
@@ -1,7 +1,18 @@
+import mailList from '../cmps/mail-list.js'
+import mailFilter from '../cmps/mail-filter.js'
+import mailSearch from '../cmps/mail-search.js'
+
 export default{
     template:`
     <section>
-        <h1>mail</h1>
+        <button>+ Compose</button>
+        <mail-search></mail-search>
+        <mail-filter></mail-filter>
+        <mail-list></mail-list>
     </section>
-    `
+    `,
+    components:
+    {
+        mailList, mailFilter,mailSearch
+    }
 }
