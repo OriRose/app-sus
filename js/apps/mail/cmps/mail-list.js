7c358a50b04723701ab7140cbf2d6efd4c6ff7aa
Ori <orirose10@gmail.com>
2021-02-24 22:16:23 +0200

    added mail display

--- js/apps/mail/cmps/mail-list.js
+++ js/apps/mail/cmps/mail-list.js
@@ -11,30 +11,44 @@ export default {
                     <button v-if="mail.isStarred" @click="toggleStar(mail)">★</button>
                     <button v-else @click="toggleStar(mail)">☆</button>
                     </li>
-                    <li>
+                    <li @click="displayMail(mail.id)">
                         {{mail.sender.name}}
                     </li>
-                    <li>
+                    <li @click="displayMail(mail.id)">
                         {{mail.subject}}
-                    </li>
-                    <li>
+                    </li @click="displayMail(mail.id)">
+                    <li @click="displayMail(mail.id)">
                         {{mail.timestamp}}
-                    </li>
+                    </li @click="displayMail(mail.id)">
                     <li>
                     <button @click="remove(mail.id)">X</button>
                     </li>
+                </ul>
+                <ul v-if="(mail.id===displayedMailIdx)">
+                    <h1>{{mail.subject}}</h1>
+                    <h2><span>{{mail.sender.name}}</span> <span>{{mail.sender.address}}</span></h2>
+                    <p>{{mail.content}}</p>
                 </ul>
             </li>
         </ul>
     </section>
     `,
+    data() {
+        return {
+            displayedMailIdx: null
+        }
+    },
     methods: {
         remove(mailId) {
             this.$emit('remove', mailId)
         },
-        toggleStar(mail){
+        toggleStar(mail) {
             mail.isStarred = !mail.isStarred
-            this.$emit('starred',mail)
+            this.$emit('starred', mail)
+        },
+        displayMail(mailId) {
+            if(mailId === this.displayedMailIdx) this.displayedMailIdx = null
+            else this.displayedMailIdx = mailId
         }
     }
     // mounted() {
