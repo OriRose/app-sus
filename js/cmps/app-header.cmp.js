78de008d5f5471e8ec3fb186385cd46ce87d0757
Ori <orirose10@gmail.com>
2021-02-24 10:11:54 +0200

    added header

---
+++ js/cmps/app-header.cmp.js
@@ -0,0 +1,13 @@
+export default {
+    template:`<section>
+        <h1>Logo</h1>
+        <nav>
+          <router-link to="/" />
+          <router-link to="/mail" />
+          <router-link to="/book" />
+          <router-link to="/keep" />
+          <router-link to="/about" />
+        </nav>
+    <section>
+    `
+}
--- js/routes.js
+++ js/routes.js
@@ -16,7 +16,7 @@ const routes = [
     {
         path:'/mail',
         component: mailApp
-    }
+    },
     {
         path:'/about',
         component: aboutPage
