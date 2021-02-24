b937bc87c36b523eff99d1188cae74e33a8cbebf
Ori <orirose10@gmail.com>
2021-02-24 09:54:21 +0200

    worked on routes

--- js/routes.js
+++ js/routes.js
@@ -1,5 +1,26 @@
+// import 
 
 const routes = [
+    {
+        path:'/',
+        component: homePage
+    },
+    {
+        path:'/book',
+        component: bookApp
+    },
+    {
+        path:'/keep',
+        component: keepApp
+    },
+    {
+        path:'/mail',
+        component: mailApp
+    }
+    {
+        path:'/about',
+        component: aboutPage
+    }
 ]
 
 export const myRouter = new VueRouter({ routes })
