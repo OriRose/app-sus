de3ef08fde02484199f1bfad884d2d7e8a0bad3f
Ori <orirose10@gmail.com>
2021-02-24 10:14:47 +0200

    more header work

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -2,11 +2,11 @@ export default {
     template:`<section>
         <h1>Logo</h1>
         <nav>
-          <router-link to="/" />
-          <router-link to="/mail" />
-          <router-link to="/book" />
-          <router-link to="/keep" />
-          <router-link to="/about" />
+          <router-link to="/" >Home</router-link>
+          <router-link to="/mail" >Mail</router-link>
+          <router-link to="/book" >Book</router-link>
+          <router-link to="/keep" >Keep</router-link>
+          <router-link to="/about" >About</router-link>
         </nav>
     <section>
     `
