5d50f12d6e919952b8816c871f653adcc5ed1ddc
Ori <orirose10@gmail.com>
2021-02-25 20:54:44 +0200

    added data to mail compose component

--- js/apps/mail/cmps/mail-compose.cmp.js
+++ js/apps/mail/cmps/mail-compose.cmp.js
@@ -2,17 +2,24 @@ export default {
     template:`
         <div>
             <form>
-                <input type="text" placeholder="To...">
-                <input type="text" placeholder="Subject">
-                <input type="text">
+                <input type="text" placeholder="To..." v-model="recepient">
+                <input type="text" placeholder="Subject" v-model="subject">
+                <input type="text" v-model="content">
                 <button @click="sendMail">Send</button>
             </form>
         </div>
     `,
+    data() {
+        return {
+            recepient:'',
+            subject:'',
+            content:''
+        }
+    },
     methods:{
         sendMail(ev){
             ev.preventDefault()
-            
+
             console.log('hi')
         }
     }
