d73d247baed2a4f0861b6d3b3d27f852ab043975
Ori <orirose10@gmail.com>
2021-02-26 17:10:08 +0200

    worked on filter

--- js/apps/mail/pages/mail-app.cmp.js
+++ js/apps/mail/pages/mail-app.cmp.js
@@ -18,7 +18,7 @@ export default {
         return {
             mails: [],
             searchString: '',
-            // filterByFolder:null
+            filterByFolder:'inbox'
         }
     },
     methods: {
