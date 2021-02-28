0e22fc9f4142f6f6e32605946c73b736c0e5a280
Ori <orirose10@gmail.com>
2021-02-28 02:43:10 +0200

    sample data changed

--- js/apps/mail/services/mail-service.js
+++ js/apps/mail/services/mail-service.js
@@ -39,11 +39,12 @@ function _createEmails() {
     if (!mails || !mails.length) {
         mails = []
         //Todo: make better demo data
-        mails.push(_createMail('Puki','pukiMcPuck@bouncy.mail','hi!','Hello',d.toLocaleString(),'inbox',false))
-        mails.push(_createMail('Shuki','shukiMcShocko@jmail.com','Greetings, human!','Welcome to universe',d.toLocaleString(),'inbox',false))
-        mails.push(_createMail('Muki','muki@amazon.mail.org','You made 153485$ this month','Revenue Report',d.toLocaleString(),'inbox',false))
+        mails.push(_createMail('Alvin Pennell','pukiMcPuck@bouncy.mail','hi!','Hello',d.toLocaleString(),'inbox',false))
+        mails.push(_createMail('Mireille Urick','shukiMcShocko@jmail.com','Greetings, human!','Welcome to the Universe',d.toLocaleString(),'inbox',false))
+        mails.push(_createMail('Tyler Stoner','muki@amazon.mail.org','You made 153485$ this month','Revenue Report',d.toLocaleString(),'inbox',false))
         mails.push(_createMail('Goliath Bank','noreply@goliathBank.org','Your stocks portfolio was updated.','Stocks Portfolio',d.toLocaleString(),'inbox',false))
         mails.push(_createMail('Spongebob','spongebob@oceanicmail.com','Your order of 1 krabby patty is on its way','Krabby Patty Order',d.toLocaleString(),'inbox',false))
+        // mails.push(_createMail('Ethelyn Tillman','noreply@spam.com','No depending be convinced in unfeeling he. Excellence she unaffected and too sentiments her. Rooms he doors there ye aware in by shall. Education remainder in so cordially. His remainder and own dejection daughters sportsmen. Is easy took he shed to kind.','Spam',d.toLocaleString(),'inbox',false))
         utilService.saveToStorage(EMAIL_KEY, mails)
     }
     return mails;
